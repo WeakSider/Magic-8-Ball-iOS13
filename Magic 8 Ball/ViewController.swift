4576a5727e80c36dbb842fb106b85ade026abdc9
WeakSider <ozdamar.huseyin@outlook.com>
2022-04-15 21:44:12 +0300

    main.storyboard is created
    Background is added.
    Label is added.
    Image is added.
    Button is added.
    Action code is written for pressing button.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButonPressed(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+        imageView.image = ballArray.randomElement()
+    }
+    
+    
 
 
 
